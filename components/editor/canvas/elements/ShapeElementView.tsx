ead2e20710317feb33c0523e699eb0737021bc5a
calvenwilliams1-pixel <291940597+calvenwilliams1-pixel@users.noreply.github.com>
2026-08-21 14:14:59 -0400

    Add ShapeElementView component for rendering shapes

---
+++ components/editor/canvas/elements/ShapeElementView.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import React from "react";
+import { CanvasElement } from "@/types/canvas";
+
+interface ShapeElementViewProps {
+  element: CanvasElement;
+}
+
+export default function ShapeElementView({ element }: ShapeElementViewProps) {
+  const common: React.CSSProperties = {
+    width: "100%",
+    height: "100%",
+    backgroundColor: element.fillColor || "#e8b84b",
+    border: `${element.borderWidth || 0}px solid ${element.borderColor || "transparent"}`,
+    opacity: element.opacity ?? 1,
+  };
+
+  switch (element.shapeType) {
+    case "circle":
+      return (
+        <div
+          style={{
+            ...common,
+            borderRadius: "50%",
+          }}
+        />
+      );
+
+    case "diamond":
+      return (
+        <div
+          style={{
+            ...common,
+            width: "70%",
+            height: "70%",
+            margin: "15%",
+            transform: "rotate(45deg)",
+          }}
+        />
+      );
+
+    case "line":
+      return (
+        <div
+          style={{
+            width: "100%",
+            height: element.thickness || 2,
+            backgroundColor: element.fillColor || "#333",
+            opacity: element.opacity ?? 1,
+          }}
+        />
+      );
+
+    default: // square
+      return (
+        <div
+          style={{
+            ...common,
+            borderRadius: element.borderRadius || 0,
+          }}
+        />
+      );
+  }
+}
