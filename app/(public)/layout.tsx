38dbbcab20bb79d531165a435f9304670c5c13a7
calvenwilliams1-pixel <291940597+calvenwilliams1-pixel@users.noreply.github.com>
2026-08-24 16:32:27 -0400

    Wrap layout in ThemeProvider for theming support

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderWrapper from "@/components/layout/HeaderWrapper";
 import Footer from "@/components/layout/Footer";
+import ThemeProvider from "@/components/theme/ThemeProvider";
 
 export const revalidate = 3600;
 
@@ -10,10 +11,12 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <HeaderWrapper />
-      <main className="flex-1">{children}</main>
-      <Footer />
-    </div>
+      <ThemeProvider>
+      <div className="min-h-screen flex flex-col">
+        <HeaderWrapper />
+        <main className="flex-1">{children}</main>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
